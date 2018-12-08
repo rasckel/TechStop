3770f5d95c36da9385275ca31b93277fe77c8e0b
AJ Ervin <ajervin@cedarville.edu>
2018-12-08 09:53:37 -0500

    add using

--- Release_Candidate_Ervin_Hostetler/ViewModel/ReportViewModel.cs
+++ Release_Candidate_Ervin_Hostetler/ViewModel/ReportViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Release_Candidate_Ervin_Hostetler.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,17 @@ namespace Release_Candidate_Ervin_Hostetler.ViewModel
 {
     public class ReportViewModel
     {
-
+        public string Name { get; set; }
+        public int TicketNumber { get; set; }
+        public List<Labor> Labor { get; set; }
+        public List<AmazonPurchase> AmazonPurchase { get; set; }
+        public List<Purchase> Purchase { get; set; }
+        public string Type { get; set; }
+        public DateTime Date { get; set; }
+        public string Reason { get; set; }
+        public double SubTotal { get; set; }
+        public double Tax { get; set; }
+        public double Total { get; set; }
+        public double ShippingCost { get; set; }
     }
 }
