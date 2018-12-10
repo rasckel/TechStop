2d7063e8605d36bc456ea6191a3691bbd5398c63
rchostetler <rchostetler@C-06MIL141-1390>
2018-12-10 10:34:05 -0500

    QuoteViewModel updated

--- Release_Candidate_Ervin_Hostetler/ViewModel/QuoteViewModel.cs
+++ Release_Candidate_Ervin_Hostetler/ViewModel/QuoteViewModel.cs
@@ -11,16 +11,25 @@ namespace Release_Candidate_Ervin_Hostetler.ViewModel
       
         public string Name { get; set; }
         public int TicketNumber { get; set; }
-        public List<Labor> Labor { get; set; }
-        public List<AmazonPurchase> AmazonPurchase { get; set; }
-        public List<Purchase> Purchase { get; set; }
+
+        public int LaborQuantity { get; set; }
+        public string LaborDescription { get; set; }
+        public double LaborCost { get; set; }
+        public int PurchaseQuantity { get; set; }
+        public string PurchaseDescription { get; set; }
+        public double PurchasePrice { get; set; }
+        public string PurchaseURL { get; set; }
+        public double PurchaseShippingCost { get; set; }
+
+        public string URL { get; set; }
+        public double ShippingCost { get; set; }
         public string Type { get; set; }
         public DateTime Date { get; set; }
         public string Reason { get; set; }
         public double SubTotal { get; set; }
         public double Tax { get; set; }
         public double Total { get; set; }
-        public double ShippingCost { get; set; }
+
 
     }
 }
